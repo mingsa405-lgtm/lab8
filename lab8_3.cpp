f03a0a5e28bb3fe9c15381f2109e5d9b9a5ffc43
ming <mingsa405@gmail.com>
2025-12-16 16:21:27 +0700

    up

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,31 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int age ;
+    double high;
+    int bon;
+   cout<< "Enter your age: ";
+   cin>>age;
+   if(age<=25){
+       cout<<"Enter your height: ";
+       cin>>high;
+       if(high<100){cout<<"Your character = "<<"Chopper";}
+       else if(high<180){cout<<"Your character = "<<"Usopp";}
+       else{
+            cout<<"Enter your bounty: ";
+            cin>>bon;
+            if(bon>1100000000)
+                 {cout<<"Your character = "<<"Zoro";}
+                 else{cout<<"Your character = "<<"Sanji";}
+         }
+       } else if(age<=60){
+            cout<<"Enter your bounty: ";
+            cin>>bon;
+            if(bon>500000000){ cout<<"Your character = " <<"Jinbe"; }
+            else { cout<<"Your character = " <<"Franky";}
+        }
+        else { cout<<"Your character = " <<"Brook";}
+
+}
